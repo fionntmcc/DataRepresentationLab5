1b1d8b9df111f1a2e5fa52afc9d5f0886bfe3e81
Fionn McCarthy <g00414386@atu.ie>
2024-10-25 10:45:38 +0000

    Add firstname and surname as url params

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+app.get('/hello/:fname/:surname', (req, res) => {
+    const fname = req.params.fname;
+    const surname = req.params.surname;
+    res.send(`Hello ${fname} ${surname}`);
+});
+
 app.get('/', (req, res) => {
     res.send('Welcome to Data Rep and Querying');
 });
@@ -14,7 +25,3 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
