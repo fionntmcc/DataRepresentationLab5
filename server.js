1f69897f4290dfbf8571baaa5078d75d64776061
Fionn McCarthy <g00414386@atu.ie>
2024-10-25 11:02:32 +0000

    index.html handles first and surname inputs and redirects to "/name"

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
 const path = require('path');
 
 app.get('/index', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+
+    app.get('/name', (req, res) => {
+        const firstname = req.query.firstname;
+        const lastname = req.query.lastname;
+        res.send(`Hello ${firstname} ${lastname}`);
+    });
 });
 
 app.use((err, req, res, next) => {
