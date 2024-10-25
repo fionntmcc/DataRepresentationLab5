ea99db47955173af9aad5938918ab146af13e5f0
Fionn McCarthy <g00414386@atu.ie>
2024-10-25 10:38:05 +0000

    Welcome messaage changed

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Welcome to Data Rep and Querying');
+});
+
+app.get("/whatever", (req, res) => {
+    res.send("Whatever I want");
 });
 
 app.listen(port, () => {
