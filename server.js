23ceae0376582bcdbec65ed0b1b2b8addfd668d6
Fionn McCarthy <g00414386@atu.ie>
2024-10-25 10:39:12 +0000

    Error handling added

--- server.js
+++ server.js
@@ -13,3 +13,8 @@ app.get("/whatever", (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
